a45980bed23eeb64e6f65d8a274f72e484c31086
steven-kid <stevenkid404@gmail.com>
2023-03-21 18:49:34 +0800

    modify

--- autoweb.py
+++ autoweb.py
@@ -2,13 +2,8 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-prefs = prefs = {
-'download.prompt_for_download': False,
-'download.extensions_to_open': 'rpm',
-'safebrowsing.enabled': True
-}
+
 opts = webdriver.ChromeOptions()
-opts.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_options=opts, executable_path='../chromedriver')
 
 driver = webdriver.Chrome('../chromedriver')
@@ -17,5 +12,3 @@ driver.find_element(By.CLASS_NAME, 'search').click()
 driver.find_element(By.CLASS_NAME, 'search').send_keys("sqlite-libs")
 driver.find_element(By.CLASS_NAME, 'btn').click()
 driver.find_element(By.XPATH, '//*[@id="mirror-search"]/div/div[3]/div[1]/div[2]/div[1]/div[5]/a').click()
-
-time.sleep(5)
